6b9ed7cbca30cebfbcd17f7a60d646be58cd8bcc
Pythofanoff <artem-selifanov@mail.ru>
2025-10-08 01:58:06 +0500

    Add files via upload
    hotfix: fix logic of lib

--- __main__.py
+++ __main__.py
@@ -1,23 +1,40 @@
-import os
+try:
+    import os, time
+
+except ImportError as e:
+    print(f"Not enough trace dependencies: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    start = time.time()
+    sep = '-' * 15
 
-    with open('.gitignore', 'w') as file: file.write('')
+    print(sep)
+    print('AutoPy start...')
+    print(sep)
 
-    with open('README.txt', 'w', encoding='UTF-8') as file: file.write('')
+    try:
 
-    with open('pyproject.toml', 'w') as file: file.write('')
+        os.makedirs("src", exist_ok=True)
+        os.makedirs("src/tests", exist_ok=True)
+        os.makedirs("src/logs", exist_ok=True)
+        os.makedirs("src/configs", exist_ok=True)
+        os.makedirs("src/functions", exist_ok=True)
+        os.makedirs("src/classes", exist_ok=True)
+        os.makedirs("src/images", exist_ok=True)
+        os.makedirs("src/cashes", exist_ok=True)
 
-    os.makedirs("src", exist_ok=True)
 
-    with open('src/main.py', 'w') as file: file.write('')
+        with open(".gitignore", "x") as file: pass
 
-    os.makedirs("src/tests", exist_ok=True)
-    os.makedirs("src/logs", exist_ok=True)
-    os.makedirs("src/configs", exist_ok=True)
-    os.makedirs("src/functions", exist_ok=True)
-    os.makedirs("src/classes", exist_ok=True)
-    os.makedirs("src/images", exist_ok=True)
-    os.makedirs("src/cashes", exist_ok=True)
+        with open("README.txt", "x", encoding="UTF-8") as file: pass
+
+        with open("pyproject.toml", "x") as file: pass
+
+        with open("src/main.py", "x") as file: pass
+
+    except FileExistsError as e: print(f"The following files already exist: {e}")
+
+    finally: print(f"{sep} \n[Spent {round(time.time() - start, 3)}]: AutoPy created the project structure successfully \n{sep}")
+
 
